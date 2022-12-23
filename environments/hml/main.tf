5e5109f0117149737dd473b067a621dae882f148
Danilo Souza <danilosilvase@gmail.com>
2022-12-23 11:39:38 +0000

    refactoring database module, add hml stages

---
+++ environments/hml/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region  = local.region
+  profile = "delbank"
+}
+
+locals {
+  name   = "delbank-infra" ## Atualize com o nome do ambiente
+  region = "us-east-1"     ## Atualize com a regiao
+  tags = {
+    Name     = local.name
+    Ambiente = "dev"
+  }
+}
+
+################################################################################
+# Computing
+################################################################################
+module "computing" {
+  source                         = "../../modules/computing"
+  vpc_hml_default_security_group = module.networking.vpc_hml_default_security_group
+  private_subnet_hlm_2           = module.networking.private_subnet_hlm_2
+}
+
+################################################################################
+# Networking
+################################################################################
+module "networking" {
+  source = "../../modules/networking"
+}
+
+
+# ################################################################################
+# # RDS Module
+# ################################################################################
+
+module "database" {
+  source                   = "../../modules/database"
+  vpc_id                   = module.networking.vpc_id_hml
+  subnet_ids               = module.networking.private_subnets_list
+  name                     = "sv-db-prod"
+  password                 = var.password
+  db_subnet_group_name     = "grupo  de sub-redes rds db prod"
+  create_subnet_group_name = false
+}
